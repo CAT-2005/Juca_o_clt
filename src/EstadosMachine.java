58ddfdb3c639caf04ef98fc628ea9facf18030b8
CAT-2005 <leobigotiba@gmail.com>
2026-09-03 20:21:08 -0300

    Projeto finalizado.

--- src/EstadosMachine.java
+++ src/EstadosMachine.java
@@ -11,23 +11,67 @@ public class EstadosMachine {
         EstadosJuca estado = EstadosJuca.Working;
 
         while(true){
-            switch(estado){
+            switch(estado) {
                 case Working:
                     juca.WorkingEstado();
 
-                    if(juca.GetFatigue() > 50){
+                    if(juca.message){
+                        juca.EnterWorking();
+                    }
+
+                    juca.message = false;
+
+                    if (juca.GetFatigue() > 50) {
+                        juca.message = true;
                         estado = EstadosJuca.Sleeping;
-                    }else if (juca.GetHunger() > 10){
+                    } else if (juca.GetHunger() > 10) {
+                        juca.message = true;
                         estado = EstadosJuca.Eating;
                     }
                     break;
                 case Eating:
                     juca.EatingEstado();
 
+                    if(juca.message){
+                        juca.EnterEating();
+                    }
 
+                    juca.message = false;
+
+                    if (juca.GetHunger() <= 0) {
+                        juca.message = true;
+                        juca.hunger = 0;
+                        juca.ExitEating();
+                        estado = EstadosJuca.Working;
+                    }
+                    break;
+
+                case Sleeping:
+                    juca.SleepingEstado();
+
+                    if(juca.message){
+                        juca.EnterSleeping();
+                    }
+
+                    juca.message = false;
+
+                    if (juca.GetFatigue() <= 0) {
+                        juca.message = true;
+                        juca.fatigue = 0;
+                        estado = EstadosJuca.Working;
+                    }
+                    break;
+            }
+            System.out.println("--------------------------");
+            juca.InfoPrint();
+            System.out.println("--------------------------\n");
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                return;
             }
         }
     }
-
-
 }
--- src/Juca.java
+++ src/Juca.java
@@ -1,32 +1,40 @@
 public class Juca {
-    private static final int FatigueMax = 50;
-    private static final int HungerMax = 10;
 
     private EstadosMachine.EstadosJuca EstadoAtual;
 
-    int fatigue = FatigueMax;
-    int hunger = HungerMax;
+    int fatigue = 0;
+    int hunger = 0;
+    boolean message = true;
 
     public void WorkingEstado(){
         EstadoAtual = EstadosMachine.EstadosJuca.Working;
-        System.out.println("Hora de ir para o trabalho!...");
         fatigue += 5;
         hunger += 2;
     }
 
     public void EatingEstado(){
         EstadoAtual = EstadosMachine.EstadosJuca.Eating;
-        System.out.println("Bateu uma fome...");
         hunger -= 5;
     }
 
+    public void EnterWorking(){
+        System.out.println("Hora de ir para o trabalho!...\n");
+    }
+
+    public void EnterEating(){
+        System.out.println("Bateu uma fome...\n");
+    }
+
     public void ExitEating(){
-        System.out.println("Ufa! Já estou cheio...");
+        System.out.println("Ufa! Já estou cheio...\n");
+    }
+
+    public void EnterSleeping(){
+        System.out.println("Bateu um sono...\n");
     }
 
     public void SleepingEstado(){
         EstadoAtual = EstadosMachine.EstadosJuca.Sleeping;
-        System.out.println("Bateu um sono...");
         fatigue -= 10;
         hunger += 1;
     }
@@ -34,8 +42,6 @@ public class Juca {
     public int GetFatigue(){return fatigue;}
 
     public int GetHunger(){return hunger;}
-
-    public EstadosMachine.EstadosJuca GetEstadoAtual(){ return EstadoAtual; }
 
     public void InfoPrint(){
         System.out.println(EstadoAtual);
